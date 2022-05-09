9d1a50af0e5964f1b75f3763c5f60341a981d41b
Daniela von Arb <daniela.vonarb@gmail.com>
2022-05-10 11:29:59 +1200

    Finish first version

--- inflate-the-unicorn/inflate-the-unicorn.js
+++ inflate-the-unicorn/inflate-the-unicorn.js
@@ -1,5 +1,41 @@
-  // -    -   -   -   -  //
- // JAVASCRIPT CARNIVAL //
+// -    -   -   -   -  //
+// JAVASCRIPT CARNIVAL //
 // -    -   -   -   -  //
 
-console.log("Inflate The Unicorn!")
+console.log('Inflate The Unicorn!')
+
+const unicorns = document.querySelectorAll('img')
+
+const unicorn1 = document.getElementById('unicorn1')
+const unicorn2 = document.getElementById('unicorn2')
+const unicorn3 = document.getElementById('unicorn3')
+
+const images = [
+  './images/unicorn-0.png',
+  './images/unicorn-1.png',
+  './images/unicorn-2.png',
+  './images/unicorn-3.png',
+]
+
+for (let i = 0; i < unicorns.length; i++) {
+  unicorns[i].onclick = unicornClicked
+}
+
+let currentImageU1 = 0
+let currentImageU2 = 0
+let currentImageU3 = 0
+
+function unicornClicked(event) {
+  let unicorn = event.target
+
+  if (unicorn == unicorn1) {
+    currentImageU1++
+    unicorn1.src = images[currentImageU1]
+  } else if (unicorn == unicorn2) {
+    currentImageU2++
+    unicorn2.src = images[currentImageU2]
+  } else if (unicorn == unicorn3) {
+    currentImageU3++
+    unicorn3.src = images[currentImageU3]
+  }
+}
