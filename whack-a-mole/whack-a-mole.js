9deca69c1a950528165f4223b378162427b4b10f
Daniela von Arb <daniela.vonarb@gmail.com>
2022-05-11 11:10:38 +1200

    Add audio

--- whack-a-mole/whack-a-mole.js
+++ whack-a-mole/whack-a-mole.js
@@ -4,15 +4,10 @@
 
 console.log('Whack-a-Mole!')
 
-// - The ability to store or get references to the cells in the HTML table.
-// - A function to randomly select which cell should show the mole.
-// - A way to show a mole in the chosen cell.
-
 const cells = document.getElementsByTagName('td')
 
 // to convert cells HTML collection into an array
 const array = Array.from(cells)
-console.log(array)
 
 // function to randomly select which cell should show the mole
 function randomMole(arr) {
@@ -21,21 +16,27 @@ function randomMole(arr) {
 
   // get random item
   const cell = arr[randomIndex]
-  cell.onclick = whackedMole
-  // add picture of mole to cell
-  return (cell.innerHTML = '<img id = "mole" src = "mole.png">')
+
+  // create img element
+  const image = document.createElement('img')
+  image.src = 'mole.png'
+  image.setAttribute('id', 'mole')
+
+  // append img element to cell
+  newCell = cell.appendChild(image)
+
+  // add onclick method
+  image.onclick = whackedMole
 }
 
 randomMole(array)
 
-// By this point you should have a mole that appears in a random cell when you load the page, and if you refresh your browser it should appear in a different cell (barring the 1/25 chance that it re-appears in the same cell again).
-
-// Now you'll need to add an [onClick()]method for the mole that calls a function (say, named `whackedMole`) when you click on the image.
-
 // The whackedMole function should remove the mole from the current cell and then, using a function that lets you re-use the code that you wrote earlier, add the mole to a new random cell.
-
 function whackedMole(event) {
   const mole = event.target
+  const audio = new Audio()
+  audio.src = 'whack-audio.wav'
+  audio.play()
   mole.remove()
   randomMole(array)
 }
