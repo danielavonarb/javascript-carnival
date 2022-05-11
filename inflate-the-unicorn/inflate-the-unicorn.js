4acc0d62bbb3e1b500e436111951eaf5c4eec90b
Daniela von Arb <daniela.vonarb@gmail.com>
2022-05-11 20:41:14 +1200

    Fix image bug

--- inflate-the-unicorn/inflate-the-unicorn.js
+++ inflate-the-unicorn/inflate-the-unicorn.js
@@ -31,11 +31,23 @@ function unicornClicked(event) {
   if (unicorn == unicorn1) {
     currentImageU1++
     unicorn1.src = images[currentImageU1]
+    if (currentImageU1 == images.length - 1) {
+      alert('I am a happy unicorn!')
+      currentImageU1 = -1
+    }
   } else if (unicorn == unicorn2) {
     currentImageU2++
     unicorn2.src = images[currentImageU2]
+    if (currentImageU2 == images.length - 1) {
+      alert('I am a happy unicorn!')
+      currentImageU2 = -1
+    }
   } else if (unicorn == unicorn3) {
     currentImageU3++
     unicorn3.src = images[currentImageU3]
+    if (currentImageU3 == images.length - 1) {
+      alert('I am a happy unicorn!')
+      currentImageU3 = -1
+    }
   }
 }
