54ff15ad9f036ea1b636e484c94676499e926616
Daniela von Arb <daniela.vonarb@gmail.com>
2022-05-11 15:23:30 +1200

    Create clothingindex function

--- dress-the-clown/dress-the-clown.js
+++ dress-the-clown/dress-the-clown.js
@@ -4,13 +4,29 @@
 
 console.log('Dress The Clown!')
 
-document.onkeydown = changeHead
+//document.onkeydown = changeHead
+document.onkeydown = clothing
 
 const head = document.getElementById('head')
 const body = document.getElementById('body')
 const shoes = document.getElementById('shoes')
 
 let headImgIndex = 0
+
+let clothingIndex = 0
+
+function clothing(event) {
+  const key = event.keyCode
+  if (key == '38') {
+    clothingIndex++
+    if (clothingIndex == 4) clothingIndex = 0
+  } else if (key == '40') {
+    if (clothingIndex == 0) clothingIndex = 4
+    clothingIndex--
+  }
+
+  console.log(clothingIndex)
+}
 
 function changeHead(event) {
   const key = event.keyCode
