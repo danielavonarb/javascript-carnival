2fe43c68edc7ffb73fb986685574c17fd4710b23
Daniela von Arb <daniela.vonarb@gmail.com>
2022-05-11 14:42:21 +1200

    Create changeHead function

--- dress-the-clown/dress-the-clown.js
+++ dress-the-clown/dress-the-clown.js
@@ -1,5 +1,27 @@
-  // -    -   -   -   -  //
- // JAVASCRIPT CARNIVAL //
+// -    -   -   -   -  //
+// JAVASCRIPT CARNIVAL //
 // -    -   -   -   -  //
 
-console.log("Dress The Clown!")
+console.log('Dress The Clown!')
+
+document.onkeydown = changeHead
+
+const head = document.getElementById('head')
+const body = document.getElementById('body')
+const shoes = document.getElementById('shoes')
+
+let headImgIndex = 0
+
+function changeHead(event) {
+  const key = event.keyCode
+  let headSrc = './images/head' + headImgIndex + '.png'
+  head.src = headSrc
+
+  if (key == '39') {
+    headImgIndex++
+    if (headImgIndex == 6) headImgIndex = 0
+  } else if (key == '37') {
+    if (headImgIndex == 0) headImgIndex = 6
+    headImgIndex--
+  }
+}
