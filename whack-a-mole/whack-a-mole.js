a28e7b2b220b352ba300b361b23e59271df35b0b
Daniela von Arb <daniela.vonarb@gmail.com>
2022-05-14 11:50:35 +1200

    Refactor code

--- whack-a-mole/whack-a-mole.js
+++ whack-a-mole/whack-a-mole.js
@@ -9,13 +9,18 @@ const cells = document.getElementsByTagName('td')
 // to convert cells HTML collection into an array
 const array = Array.from(cells)
 
+const audio = new Audio()
+audio.src = 'whack-audio.wav'
+
 // function to randomly select which cell should show the mole
 function randomMole(arr) {
   // get random index vaule
   const randomIndex = Math.floor(Math.random() * arr.length)
+  console.log(randomIndex)
 
   // get random item
   const cell = arr[randomIndex]
+  console.log(cell)
 
   // create img element
   const image = document.createElement('img')
@@ -34,8 +39,6 @@ randomMole(array)
 // The whackedMole function should remove the mole from the current cell and then, using a function that lets you re-use the code that you wrote earlier, add the mole to a new random cell.
 function whackedMole(event) {
   const mole = event.target
-  const audio = new Audio()
-  audio.src = 'whack-audio.wav'
   audio.play()
   mole.remove()
   randomMole(array)
